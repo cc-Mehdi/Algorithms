1375eaf0d2b45d98cc5e451b5b2fe8e388ff80ef
cc-mehdi <kiarashjahangir6@gmail.com>
2024-05-20 23:53:58 +0330

    Add -StickLengths- problem set

---
+++ StickLengths/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace StickLengths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your number : ");
+            int n = int.Parse(Console.ReadLine());
+            string[] sticks = new string[n];
+            sticks = Console.ReadLine().Split().ToArray();
+
+            // sort sticks
+            Array.Sort(sticks);
+
+            // get mean of sticks lengths for getting best lengths
+            int mean = int.Parse(sticks[n / 2]);
+
+            // change other sticks lengths to best lenghts
+            int coasts = 0;
+            for (int i = 0; i < sticks.Length; i++)
+                coasts += Math.Abs(mean - int.Parse(sticks[i])); // best stick lenght - each stick lengths
+
+            Console.WriteLine(coasts);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StickLengths/Readme.md
@@ -0,0 +1,2 @@
+<a href="https://cses.fi/problemset/task/1074/"> منبع سوال </a>
+<span>(زمانی که این لینک اضافه میشود سایت منبع فیلتر است)</span>
