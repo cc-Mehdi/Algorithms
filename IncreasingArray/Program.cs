7abd674b6bd035d6525bb1e0e500b404fd5eabe6
cc-mehdi <kiarashjahangir6@gmail.com>
2024-05-24 14:49:39 +0330

    Add -IncreasingArray- problem set

---
+++ IncreasingArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace IncreasingArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array items count : ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Enter each array item : ");
+            string firstLine = Console.ReadLine();
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+                arr[i] = int.Parse(firstLine.Split()[i]);
+
+            Console.WriteLine(IncreaseArray(arr));
+
+            Console.ReadKey();
+        }
+
+        static int IncreaseArray(int[] arr)
+        {
+            int steps = 0;
+
+            for (int i = 1; i < arr.Length; i++)
+                if(arr[i-1] > arr[i])
+                    steps += arr[i-1] - arr[i];
+
+            return steps;
+        }
+    }
+}
---
+++ IncreasingArray/Readme.md
@@ -0,0 +1,2 @@
+<a href="https://cses.fi/problemset/task/1094/"> منبع سوال </a>
+<span>(زمانی که این لینک اضافه میشود سایت منبع فیلتر است)</span>
