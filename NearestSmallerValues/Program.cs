bd2ca08cd5ab755da15ede21497c7869a84f2778
cc-mehdi <kiarashjahangir6@gmail.com>
2024-05-26 17:23:42 +0330

    Add -NearestSmallerValues- problem set

---
+++ NearestSmallerValues/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace NearestSmallerValues
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter the size of the array : ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Enter the array values : ");
+            string firstLine = Console.ReadLine();
+            int[] numbers = new int[n];
+
+            for (int i = 0; i < n; i++)
+                numbers[i] = int.Parse(firstLine.Split()[i]);
+
+            FindNearestSmallerValues(numbers);
+
+            Console.ReadKey();
+        }
+
+        static void FindNearestSmallerValues(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i; j >= 0; j--)
+                {
+                    if(numbers[i] > numbers[j])
+                    {
+                        Console.Write(j+1 + " ");
+                        break;
+                    }
+                    else if (j-1 < 0)
+                        Console.Write("0 ");
+                }
+            }
+        }
+    }
+}
---
+++ NearestSmallerValues/Readme.md
@@ -0,0 +1,4 @@
+<a href="https://cses.fi/problemset/task/1141/"> منبع سوال </a>
+<span>(زمانی که این لینک اضافه میشود سایت منبع فیلتر است)</span>
+![image](https://github.com/cc-Mehdi/Algorithms/assets/57840939/813edf5a-6a0d-4910-a971-39f3bcabb44f)
+
