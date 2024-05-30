8db6c9fcee1019acd729f8f59552c7979b89a158
cc-mehdi <kiarashjahangir6@gmail.com>
2024-05-30 20:08:31 +0330

    Add -CollectingNumbers- problem set

---
+++ CollectingNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CollectingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the array size : ");
+            int  n = int.Parse(Console.ReadLine());
+            Console.Write("Enter the numbers in the array : ");
+            string secondLine = Console.ReadLine();
+            int[] numbers = new int[n];
+
+            for (int i = 0; i < n; i++)
+                numbers[i] = int.Parse(secondLine.Split()[i]);
+
+            CollectingNumbersMethod(numbers, n);
+
+            Console.ReadKey();
+        }
+
+        static void CollectingNumbersMethod(int[] numbers, int n)
+        {
+            int target = 1, round = 0;
+            while(target <= n)
+            {
+                round++;
+                for (int i = 0; i < n; i++)
+                {
+                    if(numbers[i] == target)
+                        target++;
+                }
+            }
+
+            Console.WriteLine(round);
+        }
+    }
+}
---
+++ CollectingNumbers/Readme.md
@@ -0,0 +1,2 @@
+<a href="https://cses.fi/problemset/task/2216"> منبع سوال </a>
+<span>(زمانی که این لینک اضافه میشود سایت منبع فیلتر است)</span>
